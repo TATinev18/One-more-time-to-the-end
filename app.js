55706e54ab107826d257b43b08d0076326d4d3fb
Dani Kompanski <gskalchev18@codingburgas.bg>
2021-06-20 14:12:07 +0300

    Add showUsers.ejs

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const sql = require('mssql/msnodesqlv8');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
 const {config} = require('./config/database_config');
 
 (async () => {
@@ -9,6 +14,38 @@ const {config} = require('./config/database_config');
 
         const result = await connection.request().query(`SELECT * FROM Users`);
         console.log(result.recordset);
+
+        // express experiments
+
+        app.get('/', function(req, res)
+        {
+            res.render('index');
+        });
+
+        app.get('/users', function(req, res)
+        {
+            const users = [
+                {username: result.recordset[0].username, email: result.recordset[0].email},
+                {username: result.recordset[1].username, email: result.recordset[1].email},
+            ]
+            const username = result.recordset[0].username;
+            const email = result.recordset[0].email;
+            res.render('showUsers', {
+                userList : users
+                // UserName: username,
+                // Email: email,
+            });
+        });
+
+        app.get('*', function(req, res)
+        {
+            res.send('Error: Page not found');
+        });
+
+        app.listen('5500', function(req, res) {
+            console.log(' Website');
+        });
+        //
     }
     catch(err)
     {
