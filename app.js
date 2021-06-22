ad6d2ebf3930d33c27e7b2335e0349a5af34a81c
Dani Kompanski <gskalchev18@codingburgas.bg>
2021-06-22 17:54:00 +0300

    Implement error handler

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ const {config} = require('./config/database_config');
             res.render('gamePage');
         });
 
+        //show all users from the database
         app.get('/users', function(req, res)
         {
             res.render('showUsers',
@@ -51,8 +52,19 @@ const {config} = require('./config/database_config');
             );
         });
 
-        app.get('*', function(req, res)
-        {
+        // catch 404 and forward to error handler
+        app.use('*', function(req, res, next) {
+            next(createError(404));
+        });
+
+        // error handler
+        app.use(function(err, req, res, next) {
+            // set locals, only providing error in development
+            res.locals.message = err.message;
+            res.locals.error = req.app.get('env') === 'development' ? err : {};
+        
+            // render the error page
+            res.status(err.status || 500);
             res.render('error_page');
         });
 
