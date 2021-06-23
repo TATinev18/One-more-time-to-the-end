c7b65ea2efe3c0558e26a915fb089ee7728febd2
Dani Kompanski <gskalchev18@codingburgas.bg>
2021-06-23 15:10:09 +0300

    Add contact.js

---
+++ public/js/contact.js
@@ -0,0 +1,11 @@
+const contactForm = document.querySelector('.contact-form');
+
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let subject = document.getElementById('subject');
+let message = document.getElementById('message');
+
+contactForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    
+})
