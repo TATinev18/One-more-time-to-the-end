6d0e4b89e2f7a6d18f169e69a8230ea65683a3e0
Dani Kompanski <gskalchev18@codingburgas.bg>
2021-06-26 20:35:34 +0300

    Add user password hash

--- routes/user_register.js
+++ routes/user_register.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {config} = require('../config/database_config');
 const sql = require('mssql/msnodesqlv8');
+const crypto = require('crypto');
 let bodyParser = require('body-parser');
 
 let router = express.Router();
@@ -16,16 +17,20 @@ router.get('/register', function (req, res)
     res.render('Register_User');
 });
 
+const sha256 = crypto.createHash('sha256');
+
 router.post('/register', async function(req, res, next) {
     try {
         // Put data into the Sql server
         const pool = await sql.connect   (config);
+        const passHash = sha256.update(req.body.password).digest('hex');
+        
         const result = await pool.request()
             .input("username", sql.NVarChar, req.body.username)
             .input("first_name", sql.VarChar, req.body.first_name)
             .input("last_name", sql.VarChar, req.body.last_name)
             .input("email", sql.NVarChar, req.body.email)
-            .input("password", sql.VarChar, req.body.password)
+            .input("password", sql.VarChar, passHash)
             .input("age", sql.TinyInt, req.body.age)
             .query(`
                 INSERT INTO Users (username, first_name, last_name,age, email, password)
