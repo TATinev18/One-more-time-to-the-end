37c06163a631294fd5f4b8fe2def37953859707a
Dani Kompanski <gskalchev18@codingburgas.bg>
2021-06-28 18:23:45 +0300

    Add scrollToTop button

---
+++ public/js/scrollToTop.js
@@ -0,0 +1,9 @@
+const btnScrollToTop = document.querySelector('#btnScrollToTop');
+
+btnScrollToTop.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
